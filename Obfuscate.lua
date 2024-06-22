1edb6c4bd5f0aa0b94a96a0261c984bfe986e0a9
HutaoHubs <chacagaming011@gmail.com>
2024-06-22 20:13:19 +0700

    Update and rename README.md to Obfuscate.lua

---
+++ Obfuscate.lua
@@ -0,0 +1,25 @@
+local Mercury = loadstring(game:HttpGet("https://raw.githubusercontent.com/HutaoHubs/Lua/main/README.md"))()
+local GUI = Mercury:Create{
+    Name = "Hutao Hub",
+    Size = UDim2.fromOffset(500, 330),
+    Theme = Mercury.Themes.Legacy,
+    Link = "Hutaohub Hub"
+}
+local Tab = GUI:Tab{
+	Name = "Main Page",
+	Icon = "rbxassetid://17576103507"
+}
+
+Tab:Button{
+	Name = "•~Build A Boat For Treasure~•",
+	Description = "Fast AutoFarming By Hutaohub",
+	Callback = function() loadstring(game:HttpGet("https://raw.githubusercontent.com/HutaoHubs/OPAUTOFARMBYHUTAOHUBS/main/readme.txt"))() end
+     
+}
+
+Tab:Button{
+	Name = "•~Murder Mystery 2 & Flee the facility~•",
+	Description = "This Menu OP",
+	Callback = function() loadstring(game:HttpGet("https://raw.githubusercontent.com/HutaoHubs/Script-op/main/MM2.lua"))() end
+     
+}
